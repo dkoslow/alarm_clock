1fb397581b398a7cdea205f756bb684db28e75c1
Dan Koslow <dan.koslow@gmail.com>
2013-04-16 18:12:34 -0400

    Basic alarm functionality (to be expanded).

---
+++ index.js
@@ -0,0 +1,18 @@
+var spawn  = require('child_process').spawn
+  , stdin  = process.stdin
+  , stdout = process.stdout
+
+stdout.write('Wake up in this many hours: ');
+stdin.on('data', getAlarmTime);
+
+function getAlarmTime (data) {
+  var time = data * 1000 * 60 * 60;
+  setAlarm(time);
+}
+
+function setAlarm(time) {
+  setTimeout( function() {
+    spawn('open', ['http://www.youtube.com/watch?v=VgSMxY6asoE']);
+  }, time);
+  stdin.pause();
+}
